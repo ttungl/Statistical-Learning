584789843deb88360e2f0de9528ca2104179121b
Tung Thanh Le <ttungl@users.noreply.github.com>
2018-01-10 17:54:48 -0600

    Add files via upload

--- Statistical-Learning-Stanford/R session/ch4.R
+++ Statistical-Learning-Stanford/R session/ch4.R
@@ -3,13 +3,14 @@ names(Smarket)
 summary(Smarket)
 ?Smarket
 pairs(Smarket,col=Smarket$Direction)
+
 # Logistic regression
 glm.fit=glm(Direction~Lag1+Lag2+Lag3+Lag4+Lag5+Volume,
             data=Smarket,family=binomial)
 summary(glm.fit)
 glm.probs=predict(glm.fit,type="response") 
 glm.probs[1:5]
-glm.pred=ifelse(glm.probs>0.5,"Up","Down")
+glm.pred=ifelse(glm.probs>0.5,"Up","Down") 
 attach(Smarket)
 table(glm.pred,Direction)
 mean(glm.pred==Direction)
@@ -29,6 +30,7 @@ glm.probs=predict(glm.fit,newdata=Smarket[!train,],type="response")
 glm.pred=ifelse(glm.probs >0.5,"Up","Down")
 table(glm.pred,Direction.2005)
 mean(glm.pred==Direction.2005)
+summary(glm.pred)
 106/(76+106)
 
 
